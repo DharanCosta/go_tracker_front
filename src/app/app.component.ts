c6ce29ddf26be4b7cfbf2c232b077de2fe11b817
Dharan <dharancosta@gmail.com>
2024-09-16 20:21:25 -0300

    GOT-3: Initial GO Tracker frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandpageComponent } from './components/landpage/landpage.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandpageComponent, ContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
